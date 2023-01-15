e81f0b98dbb57c967a55de3e19e9a667bb298b81
imoskalev <70842071+imoskalev@users.noreply.github.com>
2023-01-15 11:48:01 +0300

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -1,10 +1,10 @@
 let bg = document.querySelector('.bg');
-let hacker = document.querySelector('.hacker');
+let wensday = document.querySelector('.wensday');
 
 document.onmousemove = (e)=>{
 	let x = e.clientX / window.innerWidth;
 	let y = e.clientY / window.innerHeight;
 
 	bg.style.transform = "translate(-" + x * 50 + "px, -" + y * 50 + "px)";
-	hacker.style.transform = "translate(-" + x * 70 + "px, -" + y * 70 + "px)";
+	wensday.style.transform = "translate(-" + x * 70 + "px, -" + y * 70 + "px)";
 };
